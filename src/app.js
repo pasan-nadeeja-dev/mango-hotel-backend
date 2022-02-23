c60f00732fdcda94f4a2fd9ce9e28ce86b6a31e1
pasan-nadeeja-dev <pasannadeeja94@gmail.com>
2022-02-24 00:19:12 +0530

    add db connection

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express, { json } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
 import appConfig from "../config/appConfig";
 
@@ -6,7 +8,22 @@ import appConfig from "../config/appConfig";
 const server = express();
 // Set constant http server port
 const server_port = appConfig.WEB_PORT;
+// Enable all cors
+server.use(cors());
+// Parse incomming request with JSON
+server.use(json());
 
-server.listen(server_port, () =>
-  console.log(`Server listening at port ${server_port}`)
-);
+// Initiate DB connection
+mongoose
+  .connect(appConfig.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connected!");
+    return server.listen(appConfig.WEB_PORT);
+  })
+  .then(() => {
+    console.log(`Server running on ${server_port}`);
+  })
+  .catch((error) => console.log(`Config error : ${error}`));
